bc93661926e126f8e8dae9148b3348d31279c5ba
Object Edge <thommorais@gmail.com>
2016-09-16 18:25:24 -0300

    adding expressjs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function());
+app.listen(3000, function(){
+	console.log("running")
+	app.use(express.static('static'));
+});
 
-app.use(express.static('/static'));
+
+module.exports = app;
