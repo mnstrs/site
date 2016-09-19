4d7b0b250875e709d6ba13b67f2bdc19270e495b
Object Edge <thommorais@gmail.com>
2016-09-19 17:55:37 -0300

    animation of letterign

--- static/assets/javascripts/main.js
+++ static/assets/javascripts/main.js
@@ -7,60 +7,37 @@ for($i = 0; $i < 25; $i++ ){
 }
 
 
+var words = ["code", "design", "create"];
+var index = 0;
+var frame = 0;
+var goingback = false;
+var wordsIndex = 0;
 
-// var
-//   typed     = document.querySelector('#typed'),
-//   words     = ["code", "design", "create"],
-//   rnd       = Math.floor((Math.random() * 3) + 1),
-//   txt       = words[rnd],w,v,t;
-//
-//
-// for (var i = 0; i < txt.length; i++) {
-//   t = document.createTextNode(txt[i]);
-//   v = document.createElement('span');
-//   v.appendChild(t);
-//   typed.appendChild(v);
-// }
-//
+function mainloop() {
+  requestAnimationFrame(mainloop);
 
-/*
-var w = document.createTextNode(ar[d]);
-var s = document.createElement('span');
-var n = s.appendChild(w);
-typed.appendChild(n);
-*/
+  frame++;
 
-var words = ["code", "design", "create"],
-    typed = document.querySelector('#typed'),
-    nu    = words.length;
-    ar    = [],
-    wr    = [];
+  var modulo = goingback ? 5 : 10
 
+  console.log(modulo);
 
-var lettering = function(words){
-  for (i = 0; i < words.length; i++) {
-    let w = words[i];
-    for (l = 0; l < w.length; l++){
-      ar.push(w[l]);
+  if (!(frame%modulo )) {
+
+    index+= goingback ? -1 : 1;
+
+    if (index<=words[wordsIndex].length) {
+      document.querySelector("#typed").innerHTML = words[wordsIndex].substr(0,index);
+    } else {
+      goingback = true;
     }
-    ar.push("-");
-  }
-  return ar;
-}
 
-var wrap = function(ar){
-  let cnt;
-  for (i = 0; i < ar.length; i++) {
-    let l = ar[i];
-    if(l !== "-"){
-      console.log(l);
-      cnt++;
-    }else{
-      console.log(cnt);
-      cnt = 0;
+    if (index==0) {
+      goingback = false;
+      wordsIndex++;
+      wordsIndex%=words.length;
     }
   }
+
 }
-
-
-wrap(lettering(words));
+mainloop()
