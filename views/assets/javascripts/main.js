3f19c35a37b5a22b5486b67ed124f9b3f72bab90
thommorais <thom@nitrodev.com>
2016-10-03 01:32:26 -0300

    adding scroll effects

--- views/assets/javascripts/main.js
+++ views/assets/javascripts/main.js
@@ -3,29 +3,59 @@ var index = 0
 var frame = 0
 var goingback = false
 var wordsIndex = 0
+var typed = document.querySelector("#typed")
+var doc = document.documentElement
 
-function mainloop()  {
+
+
+// lettering for home.
+function mainloop(){
     requestAnimationFrame(mainloop)
     frame++
     var modulo = goingback ? 5 : 10
 
     if (!(frame % modulo)) {
-
         index += goingback ? -1 : 1
-
         if (index <= words[wordsIndex].length) {
-            document.querySelector("#typed").innerHTML = words[wordsIndex].substr(0, index)
+            typed.innerHTML = words[wordsIndex].substr(0, index)
         } else {
             goingback = true
         }
-
         if (index == 0) {
             goingback = false
             wordsIndex++
             wordsIndex %= words.length
         }
     }
-
 }
 
-mainloop()
+if (typeof(typed) != 'undefined' && typed != null){
+  mainloop()
+}
+
+
+/* / on scroll effcts
+var latestKnownScrollY = 0,
+	ticking = false;
+
+function onScroll() {
+	latestKnownScrollY = window.scrollY;
+	requestTick();
+}
+
+function requestTick() {
+	if(!ticking) {
+		requestAnimationFrame(update);
+	}
+	ticking = true;
+}
+
+function update() {
+	ticking = false;
+	var currentScrollY = latestKnownScrollY;
+  console.log(currentScrollY)
+}
+
+window.addEventListener('scroll', onScroll, false);
+
+*/
