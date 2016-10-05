559764513d376ef0cf6952fce1eaea50fff6af0b
thommorais <thom@nitrodev.com>
2016-10-05 02:26:36 -0300

    hey

--- views/assets/javascripts/main.js
+++ views/assets/javascripts/main.js
@@ -6,8 +6,6 @@ var wordsIndex = 0
 var typed = document.querySelector("#typed")
 var doc = document.documentElement
 
-
-
 // lettering for home.
 function mainloop(){
     requestAnimationFrame(mainloop)
@@ -33,29 +31,49 @@ if (typeof(typed) != 'undefined' && typed != null){
   mainloop()
 }
 
+// transforming section nodeList on an array
+  // getting sections
+let sections = document.querySelectorAll("section.section")
+let sectionPosY = []
 
-/* / on scroll effcts
-var latestKnownScrollY = 0,
-	ticking = false;
+for(let s = 0; s < sections.length; s++){
+  let yPos = sections[s].getBoundingClientRect()
+  let obj = {section: sections[s].id, y:  Math.floor(yPos.top)}
+  sectionPosY.push(obj)
+}
+
+var positions = sectionPosY.map(function(position){
+  return position.y
+})
+
+console.log(positions)
+
+
+// get curent scroll position on scroll lol
+let latestKnownScrollY = 0,
+	ticking = false
 
 function onScroll() {
-	latestKnownScrollY = window.scrollY;
-	requestTick();
+	latestKnownScrollY = window.scrollY
+	requestTick()
 }
 
 function requestTick() {
 	if(!ticking) {
-		requestAnimationFrame(update);
+		requestAnimationFrame(update)
 	}
-	ticking = true;
+	ticking = true
 }
 
 function update() {
-	ticking = false;
-	var currentScrollY = latestKnownScrollY;
+	ticking = false
+	var currentScrollY = latestKnownScrollY
   console.log(currentScrollY)
+
+  if ( currentScrollY >= 200 &&  currentScrollY <= 205) {
+      var menu = document.querySelector('#menu')
+      menu.classList.add('fixed');
+  }
 }
 
-window.addEventListener('scroll', onScroll, false);
-
-*/
+window.addEventListener('scroll', onScroll, false)
