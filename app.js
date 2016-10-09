5dd1bc1cd33ef0eeb36564426de3d4753db9f851
thommorais <thom@nitrodev.com>
2016-10-08 22:46:35 -0300

    adding some portfolios

--- app.js
+++ app.js
@@ -14,8 +14,18 @@ app.listen(3000, function(){
       res.render('index');
   });
 
+
+ // portfolios
   app.get('/casa-decorativa', function (req, res) {
     res.render('casa-decorativa');
+  });
+
+  app.get('/babies-buh', function (req, res) {
+    res.render('babies-buh');
+  });
+
+  app.get('/danubio', function (req, res) {
+    res.render('danubio');
   });
 
   console.log('3000 is the magic port');
--- views/assets/javascripts/main.js
+++ views/assets/javascripts/main.js
@@ -4,9 +4,52 @@ var frame = 0
 var goingback = false
 var wordsIndex = 0
 var typed = document.querySelector("#typed")
+var home = document.querySelector("#home")
 var doc = document.documentElement
+var menu = document.querySelector('#menu')
 
-// lettering for home.
+var height = window.innerHeight
+|| doc.clientHeight
+|| document.body.clientHeight
+
+
+let elements = document.querySelectorAll('section.section')
+let count = elements.length
+var indice = []
+var lastPosition = -1
+var wHeight = window.innerHeight
+var wrap = document.querySelector('#wrap')
+// getting sizes
+function sizes(){
+    lastPosition = -1 // Force a recalculation
+    wHeight = window.innerHeight
+    var i = 0
+    for (i =0; i<count; i++){
+
+        indice[i] = indice[i] || { el: elements[i] }
+        // Reini
+
+        indice[i].el.style.display = "block"
+        indice[i].height = indice[i].el.offsetHeight
+        indice[i].start = indice[i-1] ? indice[i-1].stop : 0
+        indice[i].stop = indice[i-1] ? indice[i-1].stop + indice[i].height : indice[i].height
+        indice[i].isScroll = indice[i].el.className.indexOf("stick") < 0
+
+        // If it's sticked but higher than the screen...
+        if (indice[i].height - wHeight > 0) indice[i].gap = indice[i].height - wHeight
+
+        // Let's find a index
+        indice[i].el.style.zIndex = !indice[i].isScroll ? 10 - i : 100 - i
+
+    }
+    wrap.style.height = indice[i-1].stop + "px"
+}
+
+sizes()
+
+console.log(indice)
+
+// lettering effect.
 function mainloop(){
     requestAnimationFrame(mainloop)
     frame++
@@ -27,53 +70,7 @@ function mainloop(){
     }
 }
 
+// if has typed element,runs lettering effect
 if (typeof(typed) != 'undefined' && typed != null){
   mainloop()
 }
-
-// transforming section nodeList on an array
-  // getting sections
-let sections = document.querySelectorAll("section.section")
-let sectionPosY = []
-
-for(let s = 0; s < sections.length; s++){
-  let yPos = sections[s].getBoundingClientRect()
-  let obj = {section: sections[s].id, y:  Math.floor(yPos.top)}
-  sectionPosY.push(obj)
-}
-
-var positions = sectionPosY.map(function(position){
-  return position.y
-})
-
-console.log(positions)
-
-
-// get curent scroll position on scroll lol
-let latestKnownScrollY = 0,
-	ticking = false
-
-function onScroll() {
-	latestKnownScrollY = window.scrollY
-	requestTick()
-}
-
-function requestTick() {
-	if(!ticking) {
-		requestAnimationFrame(update)
-	}
-	ticking = true
-}
-
-function update() {
-	ticking = false
-	var currentScrollY = latestKnownScrollY
-  console.log(currentScrollY)
-
-  if ( currentScrollY >= 200 &&  currentScrollY <= 205) {
-      var menu = document.querySelector('#menu')
-      menu.classList.add('fixed');
-  }
-}
-
-window.addEventListener('scroll', onScroll, false)
